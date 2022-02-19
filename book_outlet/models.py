d9f344172c89f7cae3ad103e77193a9d2bcc3c78
József Mandula <63676153+joealmond@users.noreply.github.com>
2022-02-19 19:27:07 +0100

    Section 6 finish

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -10,9 +12,21 @@ class Book(models.Model):
         validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
+    # convert url "3" to "harry-ptter-1", add db_index for search performace (you may set this it pk also)
+    slug = models.SlugField(default="", null=False, db_index=True)
+    # id = models.AutoField() will be automatically added
 
-    # id = models.AutoField() "automatically added"
+    # For save a slug based on the title
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
-    # For outputing readable queris in the shell:
+    # For simler url call in template(by Jocc):
+    def get_absolute_url(self):
+        return reverse("book-detail-string", args=[self.slug])
+    # def get_absolute_url(self):
+    #     return reverse("book-detail", args=[self.id])
+
+    # For outputing readable queris in the shell(by Jocc):
     def __str__(self):
         return f"{self.title} ({self.rating})"
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from audioop import reverse
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from django.db.models import Avg
+
+from .models import Book
 
 # Create your views here.
+
+
+def index(request):
+    books = Book.objects.all().order_by("-rating")
+    num_books = books.count
+    avg_rating = books.aggregate(Avg("rating"))
+    return render(request, "book_outlet/index.html", {
+        "books": books,
+        "total_number_of_books": num_books,
+        "average_rating": avg_rating
+    })
+
+
+# def book_detail(request, id):
+#     # try:
+#     #     book = Book.objects.get(pk=id)  # primary key = id
+#     # except:
+#     #     raise Http404()
+#     book = get_object_or_404(Book, pk=id)  # shortcut and primary key = id
+#     return render(request, "book_outlet/book_detail.html", {
+#         "title": book.title,
+#         "author": book.author,
+#         "rating": book.rating,
+#         "is_bestseller": book.is_bestselling
+#     })
+
+
+def book_detail_string(request, slug):
+    book = get_object_or_404(Book, slug=slug)  # shortcut and primary key = id
+    return render(request, "book_outlet/book_detail.html", {
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestseller": book.is_bestselling
+    })
